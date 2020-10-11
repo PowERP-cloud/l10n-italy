1bdf95c2160a519549c4316268bf95e6a6a910b0
Antonio Maria Vigliotti <antoniomaria.vigliotti@gmail.com>
2020-10-11 14:47:47 +0200

    [FIX] account_duedates - field credit/debit not found

--- __init__.py
+++ __init__.py
@@ -1,11 +1,9 @@
-# -*- coding: utf-8 -*-
 #
-# Copyright 2017-20 - SHS-AV s.r.l. <https://www.zeroincombenze.it/>
+# Copyright 2017-20 SHS-AV s.r.l. <https://www.zeroincombenze.it>
 #
-# Contributions to development, thanks to:
-# * Antonio Maria Vigliotti <antoniomaria.vigliotti@gmail.com>
+# Copyright 2020 Didotech s.r.l. <https://www.didotech.com>
 #
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 #
 
 
