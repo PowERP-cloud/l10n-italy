be76446450b34d07ad39eeb8f67a68dc33a4b584
Antonio Maria Vigliotti <antoniomaria.vigliotti@gmail.com>
2020-11-16 10:51:29 +0100

    [IMP] Update for account_13_more

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2020
+#
+{
+    'name': 'Account Invoice 13 more',
+    'summary': 'Account Invoice 13 more',
+    'version': '12.0.1.0.2',
+    'category': 'Accounting',
+    'author': 'Powerp Srl',
+    'website': '',
+    'license': 'LGPL-3',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/account_move_view.xml',
+    ],
+    'installable': True,
+}
