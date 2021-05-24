b9c5f56bcad2f39f643266fee123db2d8d77f367
fabio giovannelli <fabio.giovannelli@didotech.com>
2021-05-24 15:22:01 +0200

    [IMP] update documentation and license

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,24 @@
+# Copyright 2021 powERP enterprise network <https://www.powerp.it>
+# Copyright 2021 SHS-AV s.r.l. <https://www.zeroincombenze.it>
+# Copyright 2021 Didotech s.r.l. <https://www.didotech.com>
 #
-# Copyright (c) 2020
+# License OPL-1 or later (https://www.odoo.com/documentation/user/12.0/legal/licenses/licenses.html#odoo-apps).
 #
 {
     'name': 'Account Move Plus',
-    'summary': 'Account move extension for Italian Localization',
     'version': '12.0.0.2.5',
     'category': 'Accounting',
-    'author': 'Didotech srl',
-    'website': 'https://www.didotech.com/',
-    'license': 'LGPL-3',
+    'summary': 'Account move extension for Italian Localization',
+    'author': 'powERP enterprise network and other partners',
+    'website': 'https://www.powerp.it',
+    'development_status': 'Beta',
+    'license': 'OPL-1',
     'depends': [
         'account',
         'base',
         'date_range_plus',
         'account_fiscal_year',
-        # 'account_invoice_entry_dates',
     ],
-    'data': [
-        'views/account_move_view.xml',
-    ],
+    'data': ['views/account_move_view.xml'],
     'installable': True,
 }
