f53207b07cac9da24c1f505ecfddb6691a9c8668
fabio giovannelli <fabio.giovannelli@didotech.com>
2021-07-22 10:43:43 +0200

    [FIX] POW 430 added OCA original module l10n_it_reverse_charge and dependencies

--- l10n_it_fatturapa_out_rc/models/account_invoice.py
+++ l10n_it_fatturapa_out_rc/models/account_invoice.py
@@ -1,5 +1,5 @@
 from odoo import api, models, _
-# from odoo.exceptions import UserError
+from odoo.exceptions import UserError
 
 
 class Invoice(models.Model):
@@ -8,6 +8,10 @@ class Invoice(models.Model):
     def generate_self_invoice(self):
         res = super(Invoice, self).generate_self_invoice()
         if self.rc_self_invoice_id:
+            rc_type = self.fiscal_position_id.rc_type_id
+            if rc_type.fiscal_document_type_id:
+                self.rc_self_invoice_id.fiscal_document_type_id =\
+                    rc_type.fiscal_document_type_id.id
             if self.fatturapa_attachment_in_id:
                 doc_id = self.fatturapa_attachment_in_id.name
             else:
@@ -21,15 +25,15 @@ class Invoice(models.Model):
             ]
         return res
 
-    # @api.multi
-    # def action_invoice_draft(self):
-    #     super().action_invoice_draft()
-    #     for inv in self:
-    #         if not inv.env.context.get("rc_set_to_draft") and \
-    #                 inv.rc_purchase_invoice_id.state in ['draft', 'cancel']:
-    #             raise UserError(_(
-    #                 "Vendor invoice that has generated this self invoice isn't "
-    #                 "validated. "
-    #                 "Validate vendor invoice before."
-    #             ))
-    #     return True
+    @api.multi
+    def action_invoice_draft(self):
+        super().action_invoice_draft()
+        for inv in self:
+            if not inv.env.context.get("rc_set_to_draft") and \
+                    inv.rc_purchase_invoice_id.state in ['draft', 'cancel']:
+                raise UserError(_(
+                    "Vendor invoice that has generated this self invoice isn't "
+                    "validated. "
+                    "Validate vendor invoice before."
+                ))
+        return True
