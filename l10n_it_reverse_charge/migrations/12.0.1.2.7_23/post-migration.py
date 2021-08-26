e4cccc3d18374cd68509756b6625c0ab607b2c48
fabio giovannelli <fabio.giovannelli@didotech.com>
2021-08-26 14:56:20 +0200

    [REF] l10n_it_reverse_charge: update module with l10n_it_vat_rc_replace source code

---
+++ l10n_it_reverse_charge/migrations/12.0.1.2.7_23/post-migration.py
@@ -0,0 +1,31 @@
+import logging
+_logger = logging.getLogger(__name__)
+
+
+def _update_account_move(cr):
+
+    cr.execute("SELECT move_id, partner_id FROM account_move_line WHERE "
+               "move_id in (SELECT id FROM public.account_move WHERE "
+               "type = 'in_invoice' AND partner_id IS NULL AND "
+               "name like 'ACE%' AND state = 'posted') "
+               "AND user_type_id = 2 GROUP BY move_id, partner_id")
+
+    records = cr.fetchall()
+    for record in records:
+        sql = "update account_move set partner_id = {partner} where " \
+              "id = {move}".format(partner=record[1], move=record[0])
+        _logger.info(sql)
+        cr.execute(sql)
+
+
+def migrate(cr, version):
+    if not version:
+        _logger.warning("Does not exist any previous version for this module. "
+                        "Skipping the migration...")
+
+        return
+
+    _update_account_move(cr)
+
+    _logger.info("Migration executed successfully. Exiting...")
+
