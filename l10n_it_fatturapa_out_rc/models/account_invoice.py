373d0b4e7a55f7fbb66c8bf0ec4a96190bca5828
Fabio Giovannelli <fabio.giovannelli@didotech.com>
2023-06-28 11:43:16 +0200

    [FIX] l10n_it_fatturapa_out_rc: fix duplicate related document into self invoice

--- l10n_it_fatturapa_out_rc/models/account_invoice.py
+++ l10n_it_fatturapa_out_rc/models/account_invoice.py
@@ -5,30 +5,6 @@ from odoo.exceptions import UserError
 class Invoice(models.Model):
     _inherit = "account.invoice"
 
-    def generate_self_invoice(self):
-        res = super(Invoice, self).generate_self_invoice()
-        if self.rc_self_invoice_id:
-            if self.fiscal_position_id.rc_type == "self":
-                self.rc_self_invoice_id.rc_fiscal_document_type_id = (
-                    self.fiscal_position_id.rc_fiscal_document_type_id.id
-                )
-            if self.fatturapa_attachment_in_id:
-                doc_id = self.fatturapa_attachment_in_id.name
-            else:
-                doc_id = self.reference if self.reference else self.number
-            self.rc_self_invoice_id.related_documents = [
-                (
-                    0,
-                    0,
-                    {
-                        "type": "invoice",
-                        "name": doc_id,
-                        "date": self.date_invoice,
-                    },
-                )
-            ]
-        return res
-
     @api.multi
     def action_invoice_draft(self):
         super().action_invoice_draft()
